ecaf0042a3f6b2f864e1ffcf1a755b4455c041bd
Harshit <164295315+Harshit1o@users.noreply.github.com>
2025-03-22 10:02:49 +0530

    Update views.py test

--- myapp/views.py
+++ myapp/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Hello, Django! this will be deployed to kubernetes, and whole process is automated!!!!!")
+    return HttpResponse("Hello, To kese ho app log!!!")
